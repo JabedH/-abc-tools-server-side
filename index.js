5b41351b484e80ee139174596254a6aa3e905e5f
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-24 16:58:00 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,28 +1,106 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectID } = require("bson");
 const app = express();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://abc-tools:BA3JOitVLJqxn5c9@cluster0.oegym.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+const ACCESS_TOKEN =
+  "0d870417fccc085ae1b594ef307d52938038639c9d1367b7ab79c1511419b55adc79650e9b8227f36029209d9d43b72899c3a17fc292073ab8e9ea769df15959";
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
     await client.connect();
     const toolsCollection = client.db("abc-tools").collection("tools");
-    console.log(toolsCollection);
+    const bookingCollection = client.db("abc-tools").collection("booking");
+    const userCollection = client.db("abc-tools").collection("users");
+
     app.get("/tools", async (req, res) => {
       const tools = await toolsCollection.find().toArray();
       res.send(tools);
+    });
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const tools = await toolsCollection.findOne(query);
+      res.send(tools);
+    });
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/booking", verifyJWT, async (req, res) => {
+      const userEmail = req.query.userEmail;
+      const decodedEmail = req.decoded.email;
+      if (userEmail === decodedEmail) {
+        const query = { userEmail: userEmail };
+        const booking = await bookingCollection.find(query).toArray();
+        res.send(booking);
+      } else {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+    });
+
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    });
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, ACCESS_TOKEN);
+      res.send({ result, token });
+    });
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
     });
   } finally {
   }
