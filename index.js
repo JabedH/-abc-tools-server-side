725b28e66a76bf1b80f21fd7a336390060d86ad2
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-28 01:59:15 +0600

    added

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oegym.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://abc-tools:DB_PASS@cluster0.oegym.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
